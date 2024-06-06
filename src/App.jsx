bab57f4e2d78e5862809cbd7fffebbcc03572c87
thushanm <122750947+thushanm@users.noreply.github.com>
2024-06-07 00:52:13 +0530

    Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {DashBord} from "./component/DashBord.jsx";
+
+function App() {
+
+
+  return (
+    <>
+<DashBord/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/DashBord.jsx
@@ -0,0 +1,13 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+export function DashBord() {
+    return (
+        <div>
+            <h1>Hello I am Chart</h1>
+
+        </div>
+    );
+}
+
